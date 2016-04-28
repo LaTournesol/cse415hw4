2e5a0fe67b57278d537cd8eb5f12bf1b83a71da5
LaTournesol <hennessyjin@gmail.com>
2016-04-28 00:47:39 -0700

    add source code

---
+++ AStar.py
@@ -0,0 +1,127 @@
+''' AStar.py
+TianYang Jin
+CSE 415, Spring 2016, University of Washington
+Instructor: S. Tanimoto.
+Assignment 3 Part II. AStar Search
+
+'''
+
+import sys
+import queue as Q
+import importlib
+
+if len(sys.argv) != 4:
+    print("Invalid parameter size.\n")
+    quit()
+
+try:
+    puzzle = sys.argv[1]
+    Problem = importlib.import_module(puzzle)
+except ImportError:
+    quit()
+
+Initial_State = importlib.import_module(sys.argv[3].split('.')[0])
+
+
+print("\nWelcome to AStar")
+COUNT = None
+BACKLINKS = {}
+
+def heuristic(s):
+    heuristic_function = sys.argv[2]
+    if heuristic_function == 'h_euclidean':
+        return Problem.h_euclidean(s)
+    elif heuristic_function == 'h_hamming':
+        return Problem.h_hamming(s)
+    elif heuristic_function == 'h_manhattan':
+        return Problem.h_manhattan(s)
+    elif heuristic_function == 'h_custom':
+        return Problem.h_custom(s)
+    else:
+        print("Invilid heuristic evaluation function.\n")
+        quit()
+
+def runAStar():
+    initial_state = Initial_State.CREATE_INITIAL_STATE()
+    print("Initial State:")
+    print(Problem.DESCRIBE_STATE(initial_state))
+    global COUNT, BACKLINKS
+    COUNT = 0
+    BACKLINKS = {}
+    IterativeAStar(initial_state)
+    print(str(COUNT)+" states examined.")
+
+def IterativeAStar(initial_state):
+    global COUNT, BACKLINKS
+
+    OPEN = Q.PriorityQueue()
+    OPEN.put((heuristic(initial_state), initial_state))
+    OPEN_CP = [initial_state]
+    CLOSED = []
+    BACKLINKS[Problem.HASHCODE(initial_state)] = -1
+
+    while not OPEN.empty():
+        (W, S) = OPEN.get()
+        g_s = W - heuristic(S)
+        OPEN_CP.remove(S)
+        CLOSED.append(S)
+
+        if Problem.GOAL_TEST(S):
+            print(Problem.GOAL_MESSAGE_FUNCTION(S))
+            backtrace(S)
+            return
+        COUNT += 1
+        if (COUNT % 32)==0:
+            print(".",end="")
+            if (COUNT % 128)==0:
+                print("COUNT = "+str(COUNT))
+                print("len(OPEN)="+str(OPEN.qsize()))
+                print("len(CLOSED)="+str(len(CLOSED)))
+        L = []
+        for op in Problem.OPERATORS:
+            #Optionally uncomment the following when debugging
+            #a new problem formulation.
+            # print("Trying operator: "+op.name)
+            if op.precond(S):
+                new_state = op.state_transf(S)
+                if not occurs_in(new_state, CLOSED) and not occurs_in(new_state, OPEN_CP):
+                    L.append(new_state)
+                    BACKLINKS[Problem.HASHCODE(new_state)] = S
+                    #Uncomment for debugging:
+                    # print(Problem.DESCRIBE_STATE(new_state))
+        # rm = []
+        # for s2 in L:
+        #     for i in range(len(OPEN_CP)):
+        #         if Problem.DEEP_EQUALS(s2, OPEN_CP[i]):
+        #             rm = s2; break
+        #
+        # if len(rm) != 0:
+        #     L.remove(rm)
+        for s in L:
+            h_s = heuristic(s)
+            OPEN.put((g_s + 1 + h_s, s))
+        OPEN_CP = OPEN_CP + L
+
+def backtrace(S):
+    global BACKLINKS
+
+    path = []
+    while not S == -1:
+        path.append(S)
+        S = BACKLINKS[Problem.HASHCODE(S)]
+    path.reverse()
+    print("Solution path: ")
+    for s in path:
+        print(Problem.DESCRIBE_STATE(s))
+    print(len(path)-1)
+    return path
+
+
+def occurs_in(s1, lst):
+    for s2 in lst:
+        if Problem.DEEP_EQUALS(s1, s2): return True
+    return False
+
+if __name__=='__main__':
+    runAStar()
+
---
+++ SlidingBlocksProblem.py
@@ -0,0 +1,191 @@
+'''EightPuzzleWithHeuristics.py
+TianYang Jin
+CSE 415, Spring 2016, University of Washington
+Instructor: S. Tanimoto.
+Assignment 3 Part II. Eight Puzzle with 4 different heuristics functions
+
+A QUIET Solving Tool problem formulation.
+QUIET = Quetzal User Intelligence Enhancing Technology.
+The XML-like tags used here serve to identify key sections of this
+problem formulation.
+
+CAPITALIZED constructs are generally present in any problem
+formulation and therefore need to be spelled exactly the way they are.
+Other globals begin with a capital letter but otherwise are lower
+case or camel case.
+'''
+
+import math
+
+#<METADATA>
+QUIET_VERSION = "0.1"
+PROBLEM_NAME = "Basic Eight Puzzle"
+PROBLEM_VERSION = "0.1"
+PROBLEM_AUTHORS = ['TianYang Jin']
+PROBLEM_CREATION_DATE = "18-APR-2016"
+PROBLEM_DESC= \
+    '''This formulation of the Basic Eight Puzzle problem uses generic
+    Python 3 constructs and has been tested with Python 3.4.
+    It is designed to work according to the QUIET tools interface.
+    '''
+#</METADATA>
+
+#<COMMON_CODE>
+TYPIES = ['1x2', '2x1', '2x2', '1x1']
+def DEEP_EQUALS(s1,s2):
+    for i in range(0, len(s1)):
+        if s1[i] != s2[i]:
+            return False
+    return True
+
+def DESCRIBE_STATE(state):
+    # Produces a textual description of a state.
+    # Might not be needed in normal operation with GUIs.
+    s = ''
+    for i in range(R_size):
+        for j in range(C_size):
+            s += str(state[C_size * i + j]) + ' '
+        s += '\n'
+    return s
+
+def HASHCODE(s):
+    '''The result should be an immutable object such as a string
+    that is unique for the state s.'''
+    result = ''
+    for i in s:
+        result += str(i) + ';'
+    return result
+
+def copy_state(s):
+    # Performs an appropriately deep copy of a state,
+    # for use by operators in creating new states.
+    news = s[:]
+    return news
+
+
+
+def can_move(s,From,To):
+    '''Tests whether it's legal to move a disk in state s
+       from the From peg to the To peg.'''
+    try:
+        if (From < 0) or (From >= N_size * N_size) or (To < 0) or (To >= N_size * N_size): return False   # Illegal indices
+        if s[From] == 0: return False   # Cannot move from an empty space
+        if s[To] != 0: return False     # Cannot move to a non empty space
+        return True
+    except (Exception) as e:
+        print(e)
+
+def move(s,From,To):
+    '''Assuming it's legal to make the move, this computes
+       the new state resulting from moving the topmost disk
+       from the From peg to the To peg.'''
+    news = copy_state(s) # start with a deep copy.
+    news[To] = s[From]
+    news[From] = 0
+    return news # return new state
+
+def goal_test(s):
+    '''If the first two pegs are empty, then s is a goal state.'''
+    return s == [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+
+def goal_message(s):
+    return "The Eight Puzzle is complete!"
+
+goal_s = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+def h_euclidean(s):
+    '''Return the euclidean distance between current state and goal state'''
+    d = 0
+    for i in range(0, len(s)):
+        d += (s[i] - goal_s[i]) ** 2
+    return math.sqrt(d)
+
+
+def h_hamming(s):
+    '''Return ith number of tiles that, in state s, are not where they should end up in the goal state.'''
+    n = 0
+    for i in range(0, len(s)):
+        if s[i] != goal_s[i]:
+            n += 1
+    return n
+
+def h_manhattan(s):
+    '''Return the 1 norm distance between current state and the goal state'''
+    d = 0
+    for e in goal_s:
+        (x1, y1) = get_2d_coord(e)
+        (x2, y2) = get_2d_coord(s.index(e))
+        d += abs(x2 - x1) + abs(y2 - y1)
+    return d
+
+def get_2d_coord(index):
+    x = index % N_size
+    y = index // N_size
+    return (x, y)
+
+
+def h_custom(s):
+    '''Return the largest absolute distance of all'''
+    d = 0
+    for i in range(len(s)):
+        if abs(s[i]-goal_s[i]) > d:
+            d = abs(s[i]-goal_s[i])
+    return d
+
+class Operator:
+    def __init__(self, name, precond, state_transf):
+        self.name = name
+        self.precond = precond
+        self.state_transf = state_transf
+
+    def is_applicable(self, s):
+        return self.precond(s)
+
+    def apply(self, s):
+        return self.state_transf(s)
+
+#</COMMON_CODE>
+
+#<COMMON_DATA>
+R_size = 5
+C_size = 4
+#</COMMON_DATA>
+
+#<INITIAL_STATE>
+#example initial states
+CREATE_INITIAL_STATE = lambda: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+#CREATE_INITIAL_STATE = lambda : [1, 0, 2, 3, 4, 5, 6, 7, 8]
+#CREATE_INITIAL_STATE = lambda : [3, 1, 2, 4, 0, 5, 6, 7, 8]
+#CREATE_INITIAL_STATE = lambda : [1, 4, 2, 3, 7, 0, 6, 8, 5]
+
+
+
+DUMMY_STATE = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+#</INITIAL_STATE>
+
+#<OPERATORS>
+
+possible_moves = {'1x2':1, '2x1':1, '2x2':1, '1x1':1}
+OPERATORS = [Operator("Move number from "+str(p)+" to "+str(q),
+                      lambda s,p=p,q=q: can_move(s,p,q),
+                      # The default value construct is needed
+                      # here to capture the values of p&q separately
+                      # in each iteration of the list comp. iteration.
+                      lambda s,p=p,q=q: move(s,p,q) )
+             for (p,q) in possible_moves]
+#</OPERATORS>
+
+#<GOAL_TEST> (optional)
+GOAL_TEST = lambda s: goal_test(s)
+#</GOAL_TEST>
+
+#<GOAL_MESSAGE_FUNCTION> (optional)
+GOAL_MESSAGE_FUNCTION = lambda s: goal_message(s)
+#</GOAL_MESSAGE_FUNCTION>
+
+#<STATE_VIS>
+if 'BRYTHON' in globals():
+    from TowersOfHanoiVisForBrython import set_up_gui as set_up_user_interface
+    from TowersOfHanoiVisForBrython import render_state_svg_graphics as render_state
+# if 'TKINTER' in globals(): from TicTacToeVisForTKINTER import set_up_gui
+#</STATE_VIS>
